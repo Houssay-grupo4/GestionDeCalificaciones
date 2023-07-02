3c69263321c662b940bbb9101c870e5b080498d9
Alejandro-Santangelo <artesaniaslarustica@gmail.com>
2023-07-02 11:21:31 -0300

    subo arreglos archivoUsuario_Directivo

--- Usuario_Directivo.py
+++ Usuario_Directivo.py
@@ -1,7 +1,3 @@
-
-
-
-
 class Directivo:
     def __init__(self):
         self.nombre = ""
@@ -28,7 +24,7 @@ class Directivo:
             alumno.menu_opciones_alumno()
                 
         elif opcion == 3:
-            import Usuario_preceptor
+            import Usuario_Preceptor
             preceptor = Usuario_Preceptor.Preceptor()
             preceptor.menu_opciones_preceptor()
                 
@@ -42,51 +38,11 @@ class Directivo:
 directivo = Directivo()
 directivo.menu_opciones_directivo()
 
-"""
-import Usuario_Profesor
-import Usuario_preceptor
-import Usuario_Alumno
 
-class Directivo:
-    def __init__(self):
-        self.nombre = ""
-        self.contraseña = ""
 
-    def menu_opciones_directivo(self):
-        print("\nSUS OPCIONES COMO DIRECTIVO SON:")
-        print("1. Acceder como PROFESOR")
-        print("2. Acceder como ALUMNO")
-        print("3. Acceder como PRECEPTOR")
-        print("4. Salir")
 
-        opcion = int(input("Ingrese el número de opción seleccionada: "))
 
-        if opcion == 1:
-            nombre = input("Ingrese su nombre de usuario: ")
-            profesor = Usuario_Profesor.Profesor()
-            profesor.menu_opciones_profesor(nombre)
-                
-        elif opcion == 2:
-            nombre = input("Ingrese su nombre de usuario: ")
-            alumno = Usuario_Alumno.Alumno()
-            alumno.menu_opciones_alumno(nombre)
-                
-        elif opcion == 3:
-            nombre = input("Ingrese su nombre de usuario: ")
-            preceptor = Usuario_Preceptor.Preceptor()
-            preceptor.menu_opciones_preceptor(nombre)
-                
-        elif opcion == 4:
-            print("Saliendo...")
-            
-        else:
-            print("Opción inválida. Por favor, seleccione una opción válida.")
-            self.menu_opciones_directivo()
 
-directivo = Directivo()
-directivo.menu_opciones_directivo()
-
-"""
 
 
 
