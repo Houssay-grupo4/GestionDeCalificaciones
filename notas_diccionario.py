9724c3026c035cca8ab357ab62917f8fb8586a53
Chiara Seco <chiaraseco7@gmail.com>
2023-07-07 11:07:40 -0300

    cambios en la accesibilidad

--- notas_diccionario.py
+++ notas_diccionario.py
@@ -1,32 +1,39 @@
+profesores = {
+    "pablo giribaldi": ["Programación I", "Base de Datos"],
+    "narciso pérez": ["Sistemas y Organizaciones", "Elementos de Matemática y Lógica"],
+    "david": ["Aproximación al Mundo del Trabajo"],
+    "antonio sciangula": ["Competencias Comunicacionales I"]
+}
+
 materias = {
-    "Programación I": [
-        {"Alejandro Santangelo": "Alumno 1", "notas": [7, 8, 9]},
-        {"Paz Santangelo": "Alumno 2", "notas": [6, 7, 8]},
-        {"Chiara Seco": "Alumno 3", "notas": [9, 9, 9]},
-    ],
-    "Base de Datos": [
-        {"Alejandro Santangelo": "Alumno 1", "notas": [8, 9, 7]},
-        {"Paz Santangelo": "Alumno 2", "notas": [7, 6, 8]},
-        {"Chiara Seco": "Alumno 3", "notas": [9, 8, 9]}
-    ],
-    "Sistemas y Organizaciones": [
-        {"Alejandro Santangelo": "Alumno 1", "notas": [7, 8, 9]},
-        {"Paz Santangelo": "Alumno 2", "notas": [8, 9, 7]},
-        {"Chiara Seco": "Alumno 3", "notas": [6, 7, 8]}
-    ],
-    "Elementos de Matemática y Lógica": [
-        {"Alejandro Santangelo": "Alumno 1", "notas": [6, 7, 8]},
-        {"Paz Santangelo": "Alumno 2", "notas": [7, 8, 7]},
-        {"Chiara Seco": "Alumno 3", "notas": [8, 9, 7]}
-    ],
-    "Aproximación al Mundo del Trabajo": [
-        {"Alejandro Santangelo": "Alumno 1", "notas": [9, 9, 9]},
-        {"Paz Santangelo": "Alumno 2", "notas": [7, 8, 9]},
-        {"Chiara Seco": "Alumno 3", "notas": [6, 7, 8]}
-    ],
-    "Competencias Comunicacionales I": [
-        {"Alejandro Santangelo": "Alumno 1", "notas": [8, 7, 6]},
-        {"Paz Santangelo": "Alumno 2", "notas": [9, 8, 9]},
-        {"Chiara Seco": "Alumno 3", "notas": [7, 8, 7]}
-    ]
+    "Programación I": {
+        "Alejandro Santangelo": {"notas": [7, 8, 9]},
+        "Paz Santangelo": {"notas": [6, 7, 8]},
+        "Chiara Seco": {"notas": [9, 9, 9]},
+    },
+    "Base de Datos": {
+        "Alejandro Santangelo": {"notas": [8, 9, 7]},
+        "Paz Santangelo": {"notas": [7, 6, 8]},
+        "Chiara Seco": {"notas": [9, 8, 9]}
+    },
+    "Sistemas y Organizaciones": {
+        "Alejandro Santangelo": {"notas": [7, 8, 9]},
+        "Paz Santangelo": {"notas": [8, 9, 7]},
+        "Chiara Seco": {"notas": [6, 7, 8]}
+    },
+    "Elementos de Matemática y Lógica": {
+        "Alejandro Santangelo": {"notas": [6, 7, 8]},
+        "Paz Santangelo": {"notas": [7, 8, 7]},
+        "Chiara Seco": {"notas": [8, 9, 7]}
+    },
+    "Aproximación al Mundo del Trabajo": {
+        "Alejandro Santangelo": {"notas": [9, 9, 9]},
+        "Paz Santangelo": {"notas": [7, 8, 9]},
+        "Chiara Seco": {"notas": [6, 7, 8]}
+    },
+    "Competencias Comunicacionales I": {
+        "Alejandro Santangelo": {"notas": [8, 7, 6]},
+        "Paz Santangelo": {"notas": [9, 8, 9]},
+        "Chiara Seco": {"notas": [7, 8, 7]}
+    }
 }
